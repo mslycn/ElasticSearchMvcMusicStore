a87b3d536e96372be5a6f706697cd933daf0f882
Phil Pursglove <phil@philippursglove.com>
2015-10-02 13:51:46 +0100

    Initial move

---
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+using Nest;
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+
+        private static ElasticClient elasticClient
+        {
+            get
+            {
+                var setting = new ConnectionSettings(new Uri("http://localhost:9200"));
+                setting.SetDefaultIndex("musicstore");
+                return new ElasticClient(setting);
+            }
+        }
+
+        public ActionResult Search(string q)
+        {
+            var result = elasticClient.Search<Album>(body => body.Query(query => query.QueryString(qs => qs.Query(q))));
+
+            var genre = new Genre { Description = "Search results for " + q, Albums = result.Documents.ToList() };
+
+            return View("Browse", genre);
+        }
+
+
+        //
+        // GET: /Store/
+
+        public ActionResult Index()
+        {
+            var genres = storeDB.Genres.OrderBy(o => o.Name).ToList();
+
+            return View(genres);
+        }
+
+        //
+        // GET: /Store/Browse?genre=Disco
+
+        public ActionResult Browse(string genre)
+        {
+            // Retrieve Genre and its Associated Albums from database
+            var genreModel = storeDB.Genres.Include("Albums")
+                .Single(g => g.Name == genre);
+
+            return View(genreModel);
+        }
+
+        //
+        // GET: /Store/Details/5
+
+        public ActionResult Details(int id)
+        {
+            var album = storeDB.Albums.Find(id);
+
+            return View(album);
+        }
+
+        //
+        // GET: /Store/GenreMenu
+
+        [ChildActionOnly]
+        public ActionResult GenreMenu()
+        {
+            var genres = storeDB.Genres.OrderBy(o => o.Name).ToList();
+
+            return PartialView(genres);
+        }
+
+        [HttpPost]
+        public JsonResult Like(int id)
+        {
+            Album album = storeDB.Albums.Find(id);
+            album.Likes = album.Likes + 1;
+            storeDB.Entry(album).State = EntityState.Modified;
+            storeDB.SaveChanges();
+
+            return Json(album.Likes);
+        }
+    }
+}
---
+++ MvcMusicStore/Controllers/StoreManagerController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+using Nest;
+
+namespace MvcMusicStore.Controllers
+{
+    //[Authorize(Roles = "Administrator")]
+    public class StoreManagerController : Controller
+    {
+        private MusicStoreEntities db = new MusicStoreEntities();
+
+        private static ElasticClient elasticClient
+        {
+            get
+            {
+                var setting = new ConnectionSettings(new Uri("http://localhost:9200"));
+                setting.SetDefaultIndex("musicstore");
+                return new ElasticClient(setting);
+            }
+        }
+
+        public ActionResult Reindex()
+        {
+            var client = elasticClient;
+
+            foreach (var album in db.Albums)
+            {
+                client.Index(album, "musicstore", "albums", album.AlbumId);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        //
+        // GET: /StoreManager/
+
+        public ViewResult Index()
+        {
+            var albums = db.Albums.Include(a => a.Genre).Include(a => a.Artist);
+            return View(albums.OrderBy(a => a.Title).ToList());
+        }
+
+        //
+        // GET: /StoreManager/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Album album = db.Albums.Find(id);
+            return View(album);
+        }
+
+        //
+        // GET: /StoreManager/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.GenreId = new SelectList(db.Genres.OrderBy(o => o.Name), "GenreId", "Name");
+            ViewBag.ArtistId = new SelectList(db.Artists.OrderBy(o => o.Name), "ArtistId", "Name");
+            return View();
+        }
+
+        //
+        // POST: /StoreManager/Create
+
+        [HttpPost]
+        public ActionResult Create(Album album)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Albums.Add(album);
+                db.SaveChanges();
+
+                elasticClient.Index(album, "musicstore", "albums", album.AlbumId);
+
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.GenreId = new SelectList(db.Genres.OrderBy(o => o.Name), "GenreId", "Name", album.GenreId);
+            ViewBag.ArtistId = new SelectList(db.Artists.OrderBy(a => a.Name), "ArtistId", "Name", album.ArtistId);
+            return View(album);
+        }
+
+        //
+        // GET: /StoreManager/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            Album album = db.Albums.Find(id);
+            ViewBag.GenreId = new SelectList(db.Genres.OrderBy(o => o.Name), "GenreId", "Name", album.GenreId);
+            ViewBag.ArtistId = new SelectList(db.Artists.OrderBy(o => o.Name), "ArtistId", "Name", album.ArtistId);
+            return View(album);
+        }
+
+        //
+        // POST: /StoreManager/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Album album)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(album).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.GenreId = new SelectList(db.Genres.OrderBy(o => o.Name), "GenreId", "Name", album.GenreId);
+            ViewBag.ArtistId = new SelectList(db.Artists.OrderBy(o => o.Name), "ArtistId", "Name", album.ArtistId);
+            return View(album);
+        }
+
+        //
+        // GET: /StoreManager/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            Album album = db.Albums.Find(id);
+            return View(album);
+        }
+
+        //
+        // POST: /StoreManager/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Album album = db.Albums.Find(id);
+            db.Albums.Remove(album);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
